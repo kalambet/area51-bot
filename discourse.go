7041e9546641466c49403f7b0063d903de6fddd8
Peter Kalambet <peter@technogeek.io>
2017-03-23 23:36:45 +0300

    Support for Discourse 1.8.0.beta8

--- discourse.go
+++ discourse.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"net/http"
 
+	"os"
+
 	"golang.org/x/net/context"
 	"google.golang.org/appengine/log"
+	"google.golang.org/appengine/urlfetch"
 )
 
 const (
@@ -38,13 +41,24 @@ type Post struct {
 }
 
 type Topic struct {
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-	Visible   bool   `json:"visible"`
-	UserID    int    `json:"user_id"`
-	Slug      string `json:"slug"`
+	ID        int          `json:"id"`
+	Title     string       `json:"title"`
+	CreatedAt string       `json:"created_at"`
+	UpdatedAt string       `json:"updated_at"`
+	Visible   bool         `json:"visible"`
+	UserID    int          `json:"user_id"`
+	Slug      string       `json:"slug"`
+	Details   *PostDetails `json:"details"`
+}
+
+type PostDetails struct {
+	CreatedBy *UserDetils `json:"created_by"`
+}
+
+type UserDetils struct {
+	ID             int    `json:"id"`
+	UserName       string `json:"username"`
+	AvatarTemplate string `json:"avatar_template"`
 }
 
 type User struct {
@@ -63,14 +77,22 @@ type DiscoursePayload struct {
 func (p *DiscoursePayload) Message(eventType string) string {
 	switch eventType {
 	case PostCreatedEventType:
-		url := fmt.Sprintf("%s/t/%s/%d/%d", p.ForumURL, p.Topic.Slug, p.Topic.ID, p.Post.ID)
-		return fmt.Sprintf("%s (%s) написал новый <a href=\"%s\">пост в \"%s\"</a>", p.User.Name, p.User.UserName, url, p.Topic.Title)
+		url := fmt.Sprintf("%s/t/%s/%d/%d", p.ForumURL, p.Post.TopicSlug, p.Post.TopicID, p.Post.ID)
+		if p.Topic != nil {
+			return fmt.Sprintf("@%s написал новый <a href=\"%s\">пост в \"%s\"</a>", p.Post.UserName, url, p.Topic.Title)
+		} else {
+			return fmt.Sprintf("@%s написал новый <a href=\"%s\">пост на форум</a>", p.Post.UserName, url)
+		}
 	case PostEditedEventType:
-		url := fmt.Sprintf("%s/t/%s/%d/%d", p.ForumURL, p.Topic.Slug, p.Topic.ID, p.Post.ID)
-		return fmt.Sprintf("%s (%s) обновил <a href=\"%s\">пост в \"%s\"</a>", p.Post.AuthorName, p.Post.UserName, url, p.Topic.Title)
+		url := fmt.Sprintf("%s/t/%s/%d/%d", p.ForumURL, p.Post.TopicSlug, p.Post.TopicID, p.Post.ID)
+		if p.Topic != nil {
+			return fmt.Sprintf("@%s обновил <a href=\"%s\">пост в \"%s\"</a>", p.Post.UserName, url, p.Topic.Title)
+		} else {
+			return fmt.Sprintf("@%s обновил <a href=\"%s\">пост на форуме</a>", p.Post.UserName, url)
+		}
 	case TopicCreatedEventType:
 		url := fmt.Sprintf("%s/t/%s/%d", p.ForumURL, p.Topic.Slug, p.Topic.ID)
-		return fmt.Sprintf("%s (%s) создал новый топик <a href=\"%s\">\"%s\"</a> на форуме", p.User.Name, p.User.UserName, url, p.Topic.Title)
+		return fmt.Sprintf("@%s создал новый топик <a href=\"%s\">\"%s\"</a> на форуме", p.Topic.Details.CreatedBy.UserName, url, p.Topic.Title)
 	}
 
 	return ""
@@ -88,6 +110,24 @@ func HandleDiscourseEvent(ctx context.Context, header http.Header, body []byte) 
 	if err != nil {
 		log.Errorf(ctx, "HandleDiscourseEvent: %s", err.Error())
 		return "", err
+	}
+
+	// If topic is not in payload get it from the Discourse directly
+	if apiKey := os.Getenv("DISCOURSE_API_KEY"); p.Post != nil && p.Topic == nil && apiKey != "" {
+		resp, err := urlfetch.Client(ctx).Get(fmt.Sprintf("%s/t/%d.json?api_key=%s&api_username=%s", p.ForumURL, p.Post.TopicID, apiKey, p.Post.UserName))
+		if err != nil {
+			log.Errorf(ctx, "HandleDiscourseEvent: problem getting Topic details: %s", err.Error())
+			return "", err
+		}
+
+		t := Topic{}
+		err = json.NewDecoder(resp.Body).Decode(&t)
+		if err != nil {
+			log.Errorf(ctx, "HandleDiscourseEvent: problem unmarshaling Topic body: %s", err.Error())
+			return "", err
+		}
+
+		p.Topic = &t
 	}
 
 	return p.Message(e), nil
